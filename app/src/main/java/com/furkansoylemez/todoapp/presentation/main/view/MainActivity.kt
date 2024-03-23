1e379ceb192bea5d32e283fd31abd5c341497d1b
Furkan Söylemez <furkansoylemez@outlook.com>
2024-03-23 11:49:43 +0300

    [17] Improve UI

--- app/src/main/java/com/furkansoylemez/todoapp/presentation/main/view/MainActivity.kt
+++ app/src/main/java/com/furkansoylemez/todoapp/presentation/main/view/MainActivity.kt
@@ -2,6 +2,10 @@ package com.furkansoylemez.todoapp.presentation.main.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.furkansoylemez.todoapp.R
 import com.furkansoylemez.todoapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,5 +17,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setNavigation()
+    }
+
+    private fun setNavigation() {
+        val toolbar = binding.toolbar
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.nav_host_fragment
+        ) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.toDoListFragment))
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressedDispatcher.onBackPressed()
+        return super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/furkansoylemez/todoapp/presentation/to_do_list/view/ToDoListAdapter.kt
+++ app/src/main/java/com/furkansoylemez/todoapp/presentation/to_do_list/view/ToDoListAdapter.kt
@@ -1,7 +1,9 @@
 package com.furkansoylemez.todoapp.presentation.to_do_list.view
 
+import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -9,13 +11,16 @@ import com.furkansoylemez.todoapp.R
 import com.furkansoylemez.todoapp.databinding.ItemToDoTaskBinding
 import com.furkansoylemez.todoapp.domain.model.ToDoTask
 
-class ToDoListAdapter(private val onCheckClick: (ToDoTask) -> Unit,private val onItemDelete: (ToDoTask) -> Unit) :
+class ToDoListAdapter(
+    private val onCheckClick: (ToDoTask) -> Unit,
+    private val onItemDelete: (ToDoTask) -> Unit
+) :
     ListAdapter<ToDoTask, ToDoListAdapter.ToDoTaskViewHolder>(DiffCallback) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoTaskViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val binding = ItemToDoTaskBinding.inflate(layoutInflater, parent, false)
-        return ToDoTaskViewHolder(binding, onCheckClick,onItemDelete)
+        return ToDoTaskViewHolder(binding, onCheckClick, onItemDelete)
     }
 
     override fun onBindViewHolder(holder: ToDoTaskViewHolder, position: Int) {
@@ -31,18 +36,28 @@ class ToDoListAdapter(private val onCheckClick: (ToDoTask) -> Unit,private val o
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(task: ToDoTask) {
-            binding.taskTitle.text = task.title
-            binding.taskDescription.text = task.description
-            binding.taskStatus.setImageResource(
-                if (task.isCompleted) R.drawable.ic_check_circle_24
-                else R.drawable.ic_check_circle_outline_24
-            )
-            binding.taskStatus.setOnClickListener {
-                onCheckClick(task)
+            binding.apply {
+                taskTitle.text = task.title
+                taskDescription.text = task.description
+                taskStatus.setImageResource(
+                    if (task.isCompleted) R.drawable.ic_check_circle_24
+                    else R.drawable.ic_check_circle_outline_24
+                )
+                val titleTextPaintFlags =
+                    if (task.isCompleted) taskTitle.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG else taskTitle.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
+                val descriptionTextPaintFlags =
+                    if (task.isCompleted) taskDescription.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG else taskDescription.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
+                taskTitle.paintFlags = titleTextPaintFlags
+                taskDescription.paintFlags = descriptionTextPaintFlags
+                taskStatus.setOnClickListener {
+                    onCheckClick(task)
+                }
+                taskDelete.setOnClickListener {
+                    onItemDelete(task)
+                }
+                taskDelete.isVisible=task.isCompleted
             }
-            binding.taskDelete.setOnClickListener {
-                onItemDelete(task)
-            }
+
         }
     }
 
